bc5d4cf4a05439bf076b63c72f72120871b8b119
Martino Parisi <144794313+martinoparisi@users.noreply.github.com>
2023-11-03 10:18:55 +0000

    aghislow

--- semaforo.c
+++ semaforo.c
@@ -2,26 +2,53 @@
 #include <unistd.h>
 
 int main() {
-  int rosso, arancione, verde;                   // creazione variabili rosso, arancione e verde
-  int setSemV(verde), setSemP(rosso);            // creazione funzione semaforo veicoli e semaforo pedoni
+    char premuto[7] = "p";
+    char semaforoAuto[12], semaforoPedoni[14];
 
-  printf("Per attraversare la strada digita \"p\" e premi \"invio\".\n");
-  
-  do {                                           // ciclo infinito
-    if (getchar() == 'p') {                      // se ricevo "p" eseguo
-      
-      setSemV(arancione);                        // semaforo veicoli arancione
-      sleep(5);                                  // aspetta 5 secondi
-      
-      setSemV(rosso);                            // semaforo veicoli rosso
-      setSemP(verde);                            // semaforo pedoni verde
-      
-      sleep(30);                                 // aspetta 30 secondi
+    system("clear");
+    printf("Auto   🟢⚪⚪\n");
+    printf("Pedoni ⚪⚪🔴\n");
+    printf("Per attraversare la strada digita \"p\" e premi \"invio\".\n");
+    
+    do {
+        if (getchar() == 'p') {
+          sleep(5);
+          system("clear");
+          
+          printf("Auto   ⚪🟠⚪\n");
+          printf("Pedoni ⚪⚪🔴\n");
+          
+          sleep(3);
+          system("clear");
 
-      setSemV(verde);                            // semaforo veicoli verde
-      setSemP(rosso);                            // semaforo pedoni rosso
-    }
-  } while(1);
-   
- return 0;                                       // termina
+          printf("Auto   ⚪⚪🔴\n");
+          printf("Pedoni ⚪⚪🔴\n");
+          
+          sleep(5);
+          system("clear");
+          
+          printf("Auto   ⚪⚪🔴\n");
+          printf("Pedoni 🟢⚪⚪\n");
+          
+          sleep(17);
+          system("clear");
+          
+          printf("Auto   ⚪⚪🔴\n");
+          printf("Pedoni ⚪🟠⚪\n");
+          
+          sleep(3);
+          system("clear");
+          
+          printf("Auto   ⚪⚪🔴\n");
+          printf("Pedoni ⚪⚪🔴\n");
+          
+          sleep(5);
+          system("clear");
+          
+          printf("Auto   🟢⚪⚪\n");
+          printf("Pedoni ⚪⚪🔴\n");
+        }
+    } while (getchar() == 'p');
+    
+    return 0;
 }
